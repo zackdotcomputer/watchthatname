56d53cda29c0c14dc3510dff8ad400cd45dd28d0
Zack Sheppard <zack@zacksheppard.com>
2021-07-18 15:48:38 +0100

    Chakra to tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,16 @@
 import { FatalErrorBoundary } from "@redwoodjs/web";
 import { RedwoodApolloProvider } from "@redwoodjs/web/apollo";
-import { ChakraProvider } from "@chakra-ui/react";
 import FatalErrorPage from "src/pages/FatalErrorPage";
 import Routes from "src/Routes";
 
 import "./index.css";
+import "./scaffold.css";
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
-    <ChakraProvider>
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
-    </ChakraProvider>
+    <RedwoodApolloProvider>
+      <Routes />
+    </RedwoodApolloProvider>
   </FatalErrorBoundary>
 );
 
